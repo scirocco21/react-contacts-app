6d73c22e8df9ae7414791c329f6bf4a247cfbae1
Sebastian <sebgertz@gmail.com>
2018-10-21 16:31:18 -0400

    refactored component to be stateless & functional

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
 
-export default class ContactList extends Component {
-  render() {
-    const people = this.props.contacts
-    return <ol className="contact-list">
+function ContactList(props) {
+  const people = props.contacts
+  return (
+    <ol className="contact-list">
       {people.map(person => (
         <li key={person.id} className='contact-list-item'>
           <div className='contact-avatar' style={{
@@ -20,5 +20,7 @@ export default class ContactList extends Component {
         </li>
       ))}
     </ol>
-  }
+  )
 }
+
+export default ContactList
