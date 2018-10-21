35407016651e6c7d33bfb5b48fc90bb76d195ba4
Sebastian <sebgertz@gmail.com>
2018-10-21 16:50:02 -0400

    refactored contacts array to be part of App component's internal state

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import React, { Component } from 'react';
 import ContactList from './ContactList'
 import * as ContactsAPI from './utils/ContactsAPI'
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <ContactList contacts={contacts}/>
+        <ContactList contacts={this.state.contacts}/>
       </div>
     );
   }
+  
 }
 
 export default App;
