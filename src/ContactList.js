6b3359ce4785568e08465b93e279d099aa39a62f
Sebastian <sebgertz@gmail.com>
2018-10-21 15:58:45 -0400

    updated contactlist to render name, email address and avatar; added classes for css styling

--- src/ContactList.js
+++ src/ContactList.js
@@ -5,7 +5,19 @@ export default class ContactList extends Component {
     const people = this.props.contacts
     return <ol className="contact-list">
       {people.map(person => (
-        <li key={person.id}>{person.name}</li>
+        <li key={person.id} className='contact-list-item'>
+          <div className='contact-avatar' style={{
+             backgroundImage: `url(${person.avatarURL})`
+           }}>
+          </div>
+          <div className='contact-details'>
+            <p>{person.name}</p>
+            <p>{person.email}</p>
+          </div>
+          <button className='contact-remove'>
+            Remove
+          </button>
+        </li>
       ))}
     </ol>
   }
