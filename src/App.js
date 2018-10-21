8e23b7756997bedb055381d2d5969458a51a4a08
Sebastian <sebgertz@gmail.com>
2018-10-21 13:36:39 -0400

    added hardcoded contacts list array with avatar urls pulled from contacts server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import * as ContactsAPI from './utils/ContactsAPI'
 
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
+
 class ContactList extends React.Component {
   render() {
     const people = this.props.contacts
@@ -16,17 +37,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]}/>
-
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Richard' },
-          { name: 'Geoff' }
-        ]}/>
+        <ContactList contacts={contacts}/>
       </div>
     );
   }
