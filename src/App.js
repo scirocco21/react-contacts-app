7f23cda109d1986b23f542439c27a8607642787c
Sebastian <sebgertz@gmail.com>
2018-10-21 17:23:15 -0400

    new feature: defined removeContact function in App.js and bound it to button in ContactsList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import ContactList from './ContactList'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  // alternatively, state could be defined inside a constructor function:
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {contacts: [...]}
+  // }
   state = {
     contacts: [
       {
@@ -25,14 +30,22 @@ class App extends Component {
       }
     ]
   }
+
+  // removeContact returns a new array without the selected item (the element to be removed). The function will be invoked by pressing a button in the child component ContactList and therefore needs to be passed down via props
+  removeContact = (contact) => {
+    this.setState((state) => ({
+     contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <ContactList contacts={this.state.contacts}/>
+        <ContactList contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/ContactList.js
+++ src/ContactList.js
@@ -4,6 +4,7 @@ function ContactList(props) {
   const people = props.contacts
   return (
     <ol className="contact-list">
+      {/* map each person in the array to a specific li */}
       {people.map(person => (
         <li key={person.id} className='contact-list-item'>
           <div className='contact-avatar' style={{
@@ -14,7 +15,7 @@ function ContactList(props) {
             <p>{person.name}</p>
             <p>{person.email}</p>
           </div>
-          <button className='contact-remove'>
+          <button className='contact-remove' onClick={() => props.onDeleteContact(person)}>
             Remove
           </button>
         </li>
@@ -22,5 +23,5 @@ function ContactList(props) {
     </ol>
   )
 }
-
+// just like regular classes, stateless functional components need to be explicitly imported/exported
 export default ContactList
