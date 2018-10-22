aebfe83581d7bee3b91c3f4f30c16cba63f6b45d
Sebastian <sebgertz@gmail.com>
2018-10-21 23:23:10 -0400

    new feature: added search form and updatequery method in ContactList component

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ContactList(props) {
-  const people = props.contacts
-  return (
-    <ol className="contact-list">
-      {/* map each person in the array to a specific li */}
-      {people.map(person => (
-        <li key={person.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{
-             backgroundImage: `url(${person.avatarURL})`
-           }}>
-          </div>
-          <div className='contact-details'>
-            <p>{person.name}</p>
-            <p>{person.email}</p>
-          </div>
-          <button className='contact-remove' onClick={() => props.onDeleteContact(person)}>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  )
+class ContactList extends Component {
+  // use Proptypes to ensure correct datatypes are handed down
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState( {query: query.trim() })
+  }
+
+  render() {
+    const people = this.props.contacts;
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts" type="text" placeholder="Search Contacts" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+        {/* map each person in the array to a specific li */}
+        {people.map(person => (
+          <li key={person.id} className='contact-list-item'>
+            <div className='contact-avatar' style={{
+               backgroundImage: `url(${person.avatarURL})`
+             }}>
+            </div>
+            <div className='contact-details'>
+              <p>{person.name}</p>
+              <p>{person.email}</p>
+            </div>
+            <button className='contact-remove' onClick={() => this.props.onDeleteContact(person)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+    )
+  }
 }
 
-// use Proptypes to ensure correct datatypes are handed down
-ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-}
-
-// just like regular classes, stateless functional components need to be explicitly imported/exported
 export default ContactList
